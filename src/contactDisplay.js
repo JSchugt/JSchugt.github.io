7d0e69fdb0bbc445100c184b53ed721b1b28300b
Jeremiah Schugt <jeremiah.r.schugt@gmail.com>
2021-06-08 20:33:44 -0500

    Add info and images to page

--- src/contactDisplay.js
+++ src/contactDisplay.js
@@ -1,6 +1,6 @@
 export const getContactDisplay = () => {
 
-    let text = "<h2>867 5309</h2>"
-
+    let text = '<h2><a href="http://www.linkedin.com/in/jeremiah-schugt">LinkedIn</a></h2>'
+    document.getElementById("pageTitle").innerHTML = "Contact"
     document.getElementById("main-display").innerHTML = text
 }
